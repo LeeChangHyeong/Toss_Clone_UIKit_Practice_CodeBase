8f926317d7db769ed89bf76faa5b84f328fd94ba
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-07-01 21:04:39 +0900

    네비게이션 바 아이템 완료

--- Toss_Clone_UIKit_Practice_CodeBase/ViewController.swift
+++ Toss_Clone_UIKit_Practice_CodeBase/ViewController.swift
@@ -8,15 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // 메시지 아이콘 버튼 설정
+    let messageIcon = UIBarButtonItem(image: UIImage(systemName: "message.fill"), style: .plain, target: self, action: #selector(goToMessageVC))
+    // 벨 아이콘 버튼 설정
+    let bellIcon = UIBarButtonItem(image: UIImage(systemName: "bell.fill"), style: .plain, target: self, action: #selector(goToMessageVC))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.title = "뷰컨트롤러 No StoryBoard"
+//        self.title = "뷰컨트롤러 No StoryBoard"
         
         // view가 최상단
         self.view.backgroundColor = .white
+        // 네비게이션 아이템 추가
+        // style: .plain -> 기본
+        // selector는 objc에서 사용했던 것
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "toss", style: .plain, target: self, action: #selector(goToProfileVC))
+        
+        navigationItem.rightBarButtonItems = [bellIcon, messageIcon]
+        
+    }
+    // 버튼 클릭했을때 사용
+    @objc fileprivate func goToProfileVC(){
+        print("press")
+        // 객체 인스턴스 생성
+        let profileVC = MessageViewController()
+        // 푸쉬한다
+        self.navigationController?.pushViewController(profileVC, animated: true)
+    }
+    
+    @objc fileprivate func goToMessageVC(){
+        print("hi")
+        
+        let messageVC = MessageViewController()
+        // 푸쉬한다
+        self.navigationController?.pushViewController(messageVC, animated: true)
     }
 
 
